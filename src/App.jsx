fbcc65afcc4b083e97efa5e72e4ec56450723c62
phatdang <phatdang19052004@gmail.com>
2025-11-06 17:43:16 +0700

    feat: xây dựng cấu trúc code cơ bản

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RootPage from "./pages/RootPage";
-import HomaPage from "./pages/HomePage";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 
-function App() {
-  const [str, setStr] = useState("");
-
-  const sayHello = () => {
-    fetch("https://task-management-be-1-ey1a.onrender.com/sayHello")
-      .then((res) => res.json())
-      .then((res) => setStr(res));
-  };
-
-  console.log(str);
-
+export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<RootPage />}>
-            <Route index element={<HomaPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
-
-export default App;
