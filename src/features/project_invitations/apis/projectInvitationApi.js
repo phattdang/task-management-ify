feb2ad0bfb3112a4ef886e0140cf80b25bb30feb
phatdang <phatdang19052004@gmail.com>
2025-12-22 12:48:43 +0700

    feat(features.invitations.apis): thêm 2 apis là answerInvitation và getInvitationByInvitedCode
    answerInvitation trả lời lời mời vào project, getInvitationByInvitedCode lấy thông tin của lời mời

--- src/features/project_invitations/apis/projectInvitationApi.js
+++ src/features/project_invitations/apis/projectInvitationApi.js
@@ -6,6 +6,9 @@ const projectInvitationsApi = {
   inviteMember: (projectId, request) =>
     axiosClient.post(`${URL}/${projectId}`, request),
   getAllInvitations: (projectId) => axiosClient.get(`${URL}/${projectId}`),
+  answerInvitation: (request) => axiosClient.post(`${URL}/answer`, request),
+  getInvitationByInvitedCode: (invitedCode) =>
+    axiosClient.get(`${URL}/code/${invitedCode}`),
 };
 
 export default projectInvitationsApi;
