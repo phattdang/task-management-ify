83251b7607bc282e19947bc64ea781679028efb4
phatdang <phatdang19052004@gmail.com>
2026-04-20 09:52:54 +0700

    feat: implement task detail sidebar with update capabilities and scaffold authentication and task management modules

---
+++ src/features/auth/index.js
@@ -0,0 +1,8 @@
+export { default as LandingPage } from './pages/LandingPage';
+export { default as LoginPage } from './pages/LoginPage';
+export { default as RegisterPage } from './pages/RegisterPage';
+export { default as VerifyEmailPage } from './pages/VerifyEmailPage';
+export { default as SetupAccountPage } from './pages/SetupAccountPage';
+export { default as CreateSitePage } from './pages/CreateSitePage';
+
+export { useGoogleAuth } from './hooks/useGoogleAuth';
